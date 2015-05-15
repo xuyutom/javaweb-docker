789894a2f315b27ee8676edcc5dd70a77e1e6440
unknown <nikog@L342.ehi.ehealth.com>
2015-05-15 16:43:59 +0800

    update tomcat script

--- conf/supervisor/supervisord_tomcat.sh
+++ conf/supervisor/supervisord_tomcat.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Source: https://confluence.atlassian.com/plugins/viewsource/viewpagesrc.action?pageId=252348917
 function shutdown()
 {
     date
@@ -16,16 +15,8 @@ echo "Starting Tomcat"
 export CATALINA_PID=/tmp/$$
 export JAVA_HOME=/usr/java/default
 export JAVA_OPTS="-XX:PermSize=256m -XX:MaxPermSize=256m -Xms128m -Xmx256m"
-#export LD_LIBRARY_PATH=/usr/local/apr/lib
-#export JAVA_OPTS="-Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.password.file=/etc/tomcat.jmx.pwd -Dcom.sun.management.jmxremote.access.file=/etc/tomcat.jmxremote.access -Dcom.sun.management.jmxremote.ssl=false -Xms128m -Xmx3072m -XX:MaxPermSize=256m"
 
-# Uncomment to increase Tomcat's maximum heap allocation
-# export JAVA_OPTS=-Xmx512M $JAVA_OPTS
-
-. /data/tomcat/bin/catalina.sh start
-
-# Allow any signal which would kill a process to stop Tomcat
-trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
+/data/tomcat/bin/catalina.sh start
 
 echo "Waiting for `cat $CATALINA_PID`"
 wait `cat $CATALINA_PID`
